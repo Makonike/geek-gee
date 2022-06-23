f3ed1dd3af4294aa2b289a5b04cd32b707e766a4
Makonike <1694585033@qq.com>
2022-06-23 09:43:28 +0800

    feat: add pattern match

--- gee/router.go
+++ gee/router.go
@@ -3,21 +3,73 @@ package gee
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandlerFunc)}
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+// 解析路径
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, v := range vs {
+		if v != "" {
+			parts = append(parts, v)
+			// 模糊匹配剪枝，匹配的路径到此，剩下会作为参数
+			if v[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
 
 // addRoute 添加路由
 func (r *router) addRoute(method, pattern string, handlerFunc HandlerFunc) {
-	log.Printf("Route: %4s - %s", method, pattern)
+	parts := parsePattern(pattern)
 	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handlerFunc
+	log.Printf("Route: %4s - %s", method, pattern)
+}
+
+func (r *router) getRoute(method, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	// not this route
+	if !ok {
+		return nil, nil
+	}
+	n := root.search(searchParts, 0)
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for i, p := range parts {
+			if p[0] == ':' {
+				params[p[1:]] = searchParts[i]
+			}
+			if p[0] == '*' && len(p) > 1 {
+				params[p[1:]] = strings.Join(searchParts[i:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+	return nil, nil
 }
 
 func (r *router) handle(c *Context) {
---
+++ gee/trie.go
@@ -0,0 +1,80 @@
+package gee
+
+import "strings"
+
+type node struct {
+	// pattern 路径, /p/:lang
+	pattern string
+	// part 路径的一部分 :lang
+	part string
+	// children 子节点 [doc, tutorial, intro]
+	children []*node
+	// 是否精确匹配 contain [':', '*'] will be true
+	isWild bool
+}
+
+// matchChild 第一个匹配成功的结点，用于插入
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		// TODO: 如果此处都是[':', '*']，就会都匹配第一个？
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+// matchChildren 匹配当前结点的孩子，用于查找
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+// height: this node in pattern index
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+	part := parts[height]
+	child := n.matchChild(part)
+	// 没找到就新增一个结点存储
+	if child == nil {
+		child = &node{
+			pattern:  "",
+			part:     part,
+			children: nil,
+			isWild:   parts[0] == "*" || parts[0] == ":",
+		}
+		n.children = append(n.children, child)
+	}
+	// 递归插入
+	child.insert(pattern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+
+	// 匹配查找
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+	part := parts[height]
+	// get this node children
+	children := n.matchChildren(part)
+	for _, child := range children {
+		// 递归
+		res := child.search(parts, height+1)
+		if res != nil {
+			return res
+		}
+	}
+	return nil
+}
