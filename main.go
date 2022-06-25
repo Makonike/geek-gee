d7704e8528c858a7ae421d3f4d2f1f25db31e463
Makonike <1694585033@qq.com>
2022-06-25 12:39:18 +0800

    feat: add main func content

--- main.go
+++ main.go
@@ -15,5 +15,19 @@ func main() {
 		}
 		c.JSON(http.StatusOK, res)
 	})
+	r.GET("/hello", func(c *gee.Context) {
+		//fmt.Fprintf(c.Writer, "URL.Path = %q\n", c.Req.URL.Path)
+		res := make([]gee.H, 0)
+		for k, v := range c.Req.Header {
+			res = append(res, gee.H{k: v})
+		}
+		c.JSON(http.StatusOK, res)
+	})
+	r.GET("/hello/:name", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+	r.GET("/asserts/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	})
 	r.Run(":9999")
 }
